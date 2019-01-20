c578ad0053a5aafa3d3491a8e681abcdbab70c8c
Joseph Umano <joeumano@gmail.com>
2019-01-20 17:16:03 -0500

    Create Range class.

--- RegexTextParser/Expression.cs
+++ RegexTextParser/Expression.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace RegexTextParser
 {
-    class Expression
+    public static class Expression
     {
+
+        public static Range[] Plus(string text, string target)
+        {
+            if (!text.Contains(target))
+                return null;
+
+            for(int i = 0; i < text.Length; i++)
+            {
+                if (i > text.Length - 1 - target.Length)
+                    break;
+
+
+            }
+        }
     }
 }
