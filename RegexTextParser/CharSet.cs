2a4e0a9845ee0a206a4450ae8da4df47cdac091b
Joseph Umano <joeumano@gmail.com>
2019-01-22 19:47:06 -0500

    Implement CharSet ExtractRanges method.

--- RegexTextParser/CharSet.cs
+++ RegexTextParser/CharSet.cs
@@ -8,11 +8,65 @@ namespace RegexTextParser
 {
     public class CharSet
     {
-        public List<char> Characters;
+        public List<char> Characters { get; }
 
         public CharSet(string text)
         {
+            Characters = GetCharacters(text);
+        }
+
+
+        private List<char> GetCharacters(string s)
+        {
+            List<char> result = new List<char>();
+            string text = s;
+            if (text.Contains('-'))
+            {
+                result.AddRange(ExtractRanges(ref text));
+            }
+        }
+
+        private List<char> ExtractRanges(ref string text)
+        {
+            List<char> result = new List<char>();
+            List<char> letters = text.ToList();
+            while (letters.Contains('-'))
+            {
+                int index = 0;
+                while (letters[index] != '-') index++;
+                int left = index - 1;
+                int right = index + 1;
+                string start = "";
+                string end = "";
+                if (Char.IsNumber(letters[left]))
+                {
+                    start = letters[left].ToString();
+                    end = letters[right].ToString();
+                    left--;
+                    right++;
+                    while (left > 0 && Char.IsNumber(letters[left]))
+                    {
+                        start = letters[left] + start;
+                        left--;
+                    }
+                    while (right < text.Count() - 1 && Char.IsNumber(letters[right]))
+                    {
+                        end += letters[right];
+                        right++;
+                    }
+                }
+                result.AddRange(GetRanges(start, end));
+                int count = (left == 0) ? right + 1 : right - left + 1; 
+                letters.RemoveRange(left, count);
+            }
+            text = letters.ToString();
+            return result;
+        }
+
+        private List<char> GetRanges(string left, string right)
+        {
 
         }
+
     }
 }
