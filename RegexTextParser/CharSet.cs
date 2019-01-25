9f2db35c5086330c833ea2b7167a21777f02021d
Joseph Umano <joeumano@gmail.com>
2019-01-24 19:49:21 -0500

    Migrate common string functions to static StringFunction class.

--- RegexTextParser/CharSet.cs
+++ RegexTextParser/CharSet.cs
@@ -9,10 +9,7 @@ namespace RegexTextParser
     public class CharSet
     {
         public List<string> Characters { get; }
-        readonly char[] alpha = new char[]
-        {
-            'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
-        };
+        
 
         public CharSet(string text)
         {
@@ -33,125 +30,7 @@ namespace RegexTextParser
             return result;
         }
 
-        private List<string> ExtractRanges(ref string text)
-        {
-            List<string> result = new List<string>();
-            List<char> letters = text.ToList();
-            while (letters.Contains('-'))
-            {
-                int index = 0;
-                while (letters[index] != '-') index++;
-                int left = index - 1;
-                int right = index + 1;
-                if(left < 0 || right > letters.Count - 1)
-                {
-                    result.Add(letters[index].ToString());
-                    letters.RemoveAt(index);
-                    continue;
-                }
-                string start = letters[left].ToString();
-                string end = letters[right].ToString();
-                if (Char.IsNumber(letters[left]) && Char.IsNumber(letters[right]))
-                {                           
-                    left--;
-                    right++;
-                    while (left > 0 && Char.IsNumber(letters[left]))
-                    {
-                        start = letters[left] + start;
-                        left--;
-                    }
-                    while (right < text.Count() - 1 && Char.IsNumber(letters[right]))
-                    {
-                        end += letters[right];
-                        right++;
-                    }
-                }
-                if(IsValidRange(start, end))
-                {
-                    result.AddRange(GetRanges(start, end));
-                    int count = (left == 0) ? right + 1 : right - left + 1;
-                    letters.RemoveRange(left, count);
-                }
-                else
-                {
-                    result.Add(letters[index].ToString());
-                    letters.RemoveAt(index);
-                }
-            }
-            text = letters.ToString();
-            return result;
-        }
-
-        private bool IsValidRange(string start, string end)
-        {
-            if (IsNumericString(start) != IsNumericString(end))
-                return false;
-            if (Char.IsUpper(start[0]) != Char.IsUpper(end[0]))
-                return false;
-            if(IsNumericString(start))
-            {
-                int low = Convert.ToInt32(start);
-                int high = Convert.ToInt32(end);
-                return low < high;
-            }
-            // alpha
-            char left = '\0';
-            char right = '\0';
-            if (!Char.TryParse(start, out left) || !Char.TryParse(end, out right))
-                return false;
-            for(int i = 0; i < alpha.Length; i++)
-            {
-                if (alpha[i] == right)
-                    return false;
-                else if (alpha[i] == left)
-                    return true;
-            }
-            return true;
-        }
-
-        private bool IsNumericString(string num)
-        {
-            foreach(char c in num)
-            {
-                if (!Char.IsNumber(c))
-                    return false;
-            }
-            return true;
-        }
-
-        private List<string> GetRanges(string left, string right)
-        {
-            List<string> result = new List<string>();
-            if (IsNumericString(left))
-            {
-                int start = Convert.ToInt32(left);
-                int end = Convert.ToInt32(right);
-                for (int i = start; i <= end; i++)
-                    result.Add(i.ToString());
-                return result;
-            }
-            char l = '\0';
-            char r = '\0';
-
-            if (!Char.TryParse(left, out l) || !Char.TryParse(right, out r))
-                throw new ArgumentException();
-            if (Char.IsUpper(l) != Char.IsUpper(r))
-                throw new ArgumentException();
-
-            bool found = false;
-            for(int i = 0; i < alpha.Length; i++)
-            {
-                if (alpha[i] == l || Char.ToUpper(alpha[i]) == l)
-                    found = true;
-                if (!found)
-                    continue;
-                if (Char.IsUpper(l))
-                    result.Add(Char.ToUpper(alpha[i]).ToString());
-                else
-                    result.Add(alpha[i].ToString());
-            }
-            return result;
-        }
+        
 
     }
 }
--- RegexTextParserTests/ComponentTests.cs
+++ RegexTextParserTests/ComponentTests.cs
@@ -11,12 +11,22 @@ namespace RegexTextParserTests
         [TestMethod]
         public void CreateCharSetWithLiterals()
         {
-            List<CharSet> sets = new List<CharSet>
+            string[] text = new string[]
             {
-                new CharSet("abcdefg"),
-                new CharSet("1234567"),
-                new CharSet("")
+                "abcdefg",
+                "1234567",
+                "-12345",
+                "123454-",
+                "-abcdefg",
+                "abcdefg-",
+                "&*(^^*)-%*@!&*)%",
+                "*()fhdsjkPIUHGDFu1543789"
             };
+            List<CharSet> sets = new List<CharSet>();
+            foreach (string s in text)
+                sets.Add(new CharSet(s));
+            for (int i = 0; i < text.Length; i++)
+                Assert.AreEqual(text[i], string.Join("", sets[i].Characters.ToArray()));
         }
     }
 }
