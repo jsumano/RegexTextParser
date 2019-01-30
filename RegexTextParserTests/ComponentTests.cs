61e708f999aae469f426c4a3d7331856fe3c1e62
Joseph Umano <joeumano@gmail.com>
2019-01-30 17:51:49 -0500

    Create CharSet tests.

--- RegexTextParserTests/ComponentTests.cs
+++ RegexTextParserTests/ComponentTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RegexTextParser;
 
@@ -11,7 +12,37 @@ namespace RegexTextParserTests
         [TestMethod]
         public void CreateCharSetWithLiterals()
         {
-            
+            List<string> expected = new List<string>
+            {
+                "a","b","c","D","E","F","G","1","2","3"
+            };
+
+            CharSet charSet = new CharSet("abcDEFG123");
+            Assert.IsTrue(expected.SequenceEqual(charSet.Characters));
+        }
+
+        [TestMethod]
+        public void CreateCharSetWithRanges()
+        {
+            List<string> expected = new List<string>
+            {
+                "a","b","c","D","E","F","G","1","2","3"
+            };
+
+            CharSet charSet = new CharSet("a-cD-G1-3");
+            Assert.IsTrue(expected.SequenceEqual(charSet.Characters));
+        }
+
+        [TestMethod]
+        public void CreateCharSetWithRangesandLiterals()
+        {
+            List<string> expected = new List<string>
+            {
+                "D","E","F","G","a","b","c","1","2","3"
+            };
+
+            CharSet charSet = new CharSet("abcD-G123");
+            Assert.IsTrue(expected.SequenceEqual(charSet.Characters));
         }
     }
 }
