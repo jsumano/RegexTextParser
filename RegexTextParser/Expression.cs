beb132ca841e5d9119c6eeec0d26a664f3512f2f
Joseph Umano <joeumano@gmail.com>
2019-02-18 18:02:05 -0500

    Complete pattern matching loops for MinMax Expression.

--- RegexTextParser/Expression.cs
+++ RegexTextParser/Expression.cs
@@ -13,17 +13,28 @@ namespace RegexTextParser
         {
             // TODO complete implementation. Migrate common pattern matching functions to separate methods.
             // Change for loop conditional to i < text.Length - minimum pattern length.
+            int minPatternLength = 0;
+            foreach (Pattern pn in pattern)
+                minPatternLength += pn.MinimumLength;
             List<Range> result = new List<Range>();
             for(int i = 0; i < text.Length; i++)
             {
-                foreach(Pattern p in pattern)
+                int index = i;
+                for (int j = 0; j < pattern.Length; j++)
                 {
+                    if (index > text.Length - 1)
+                        break;
                     bool match = false;
-                    if (p.Type == PatternType.Literal)
-                        match = p.Match(new string[] { text.Substring(i, p.GetLiteralLength()) });
+                    if (pattern[j].Type == PatternType.Literal)
+                    {
+                        match = pattern[j].Match(new string[] { text.Substring(index, pattern[j].GetLiteralLength()) });
+                        index += pattern[j].GetLiteralLength();
+                    }
                     else
                     {
-
+                        int lastMatch = pattern[j].GetLastCharSetMatchIndex(text.Substring(index));
+                        match = index != lastMatch || pattern[j].Match(new string[] { text[index].ToString() });
+                        index = lastMatch + 1;
                     }
                     if (!match)
                         break;
