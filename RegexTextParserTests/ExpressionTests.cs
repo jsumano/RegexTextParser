5883cefd827355b14187460916bb879e510f723b
Joseph Umano <joeumano@gmail.com>
2019-02-28 19:49:56 -0500

    Create MinMax min 1 max 1 - n test for CharSet pattern.

--- RegexTextParserTests/ExpressionTests.cs
+++ RegexTextParserTests/ExpressionTests.cs
@@ -78,6 +78,17 @@ namespace RegexTextParserTests
         }
 
         [TestMethod]
+        public void MinMaxInvalidMinLargerThanMaxReturnsnull()
+        {
+            string text = "x xx xxx xxxx";
+
+            Pattern[] pattern = new Pattern[] { new Pattern("x") };
+
+            Assert.IsNull(Expression.MinMax(text, pattern, 1, 0));
+ 
+        }
+
+        [TestMethod]
         public void MinMaxnullInputsReturnsnull()
         {
             string text = "x xx xxx xxxx";
@@ -86,6 +97,32 @@ namespace RegexTextParserTests
 
             Assert.IsNull(Expression.MinMax(null, pattern, 0, 0));
             Assert.IsNull(Expression.MinMax(text, null, 0, 0));
+        }
+
+        [TestMethod]
+        public void MinMaxValidMin1ValidMax1ThroughnCharSet()
+        {
+            Pattern[] pattern = new Pattern[] { new Pattern(new CharSet("a-g4")) };
+            string text = "fees for cabs of 44 or more";
+
+            List<Range[]> expected = new List<Range[]>
+            {
+                new Range[] { new Range(5,5), new Range(15,15), new Range(25,25)},
+                new Range[] { new Range(5,5), new Range(15,15), new Range(17, 18), new Range(25,25)},
+                new Range[] { new Range(5,5), new Range(9,11), new Range(15,15), new Range(17, 18), new Range(25,25)},
+                new Range[] { new Range(0, 3), new Range(5,5), new Range(9,11), new Range(15,15), new Range(17, 18), new Range(25,25)}
+            };
+
+            for (int i = 0; i < expected.Count; i++)
+            {
+                Range[] actual = Expression.MinMax(text, pattern, 1, i + 1);
+                for (int j = 0; j < actual.Length; j++)
+                {
+                    Assert.AreEqual(expected[i][j].Left, actual[j].Left);
+                    Assert.AreEqual(expected[i][j].Right, actual[j].Right);
+                }
+
+            }
         }
     }
 }
